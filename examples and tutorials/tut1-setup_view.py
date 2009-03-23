e0f26b7c8b98f84a77e9de3c0954de3d308dd28c
RoeBros <RoeBros@29c87561-914b-0410-992d-b37967ad6221>
2009-03-23 16:58:19 +0000

    Added first tutorial.

---
+++ examples and tutorials/tut1-setup_view.py
@@ -0,0 +1,33 @@
+"""tut1-setup_view.py
+
+This tutorial shows you how to setup a basic view, start a loop, limit FPS and quit when events are grabbed."""
+
+import get_pyggel #this just makes sure we grab pyggel - so if we are running from the install directory it still works ;)
+
+import pyggel
+from pyggel import *
+
+def main():
+    pyggel.init(screen_size=(640,480)) #initialize everything
+    #for now, pyggel.init just calls pyggel.view init...
+
+    event_handler = pyggel.event.Handler()
+
+    clock = pygame.time.Clock() #pyggel automatically imports OpenGL/Pygame
+                                #for a full list of everything included,
+                                #look in pyggel/include.py
+
+    while 1:
+        clock.tick(60) #limit FPS
+        pyggel.view.set_title("FPS: %s"%int(clock.get_fps()))
+
+        event_handler.update() #get the events!
+
+        if event_handler.quit or K_ESCAPE in event_handler.keyboard.hit: #were the quit 'X' box on the window or teh ESCAPE key hit?
+           pyggel.quit() #close the window and clean up everything
+           return None #close the loop
+
+        pyggel.view.clear_screen() #clear screen for new drawing...
+        pyggel.view.refresh_screen() #flip the display buffer so anything drawn now appears
+
+main()
