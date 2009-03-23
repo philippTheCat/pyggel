c64fafe60bb01308bdc79096adccb6c0a4eb3789
RoeBros <RoeBros@29c87561-914b-0410-992d-b37967ad6221>
2009-03-23 19:18:33 +0000

    Update tut1.

--- examples_and_tutorials/tut1-setup_view.py
+++ examples_and_tutorials/tut1-setup_view.py
@@ -8,8 +8,20 @@ import pyggel
 from pyggel import *
 
 def main():
+    """pyggel.view.init can take several args that handle how the view is set up.
+       screen_size is the 2d size of the screen to create
+       screen_size_2d is the 2d size of the screen that the 2d uses,
+           the 2d elements are handled as if this is the real screen size,
+           and then scaled to fit the real screen size at render time,
+           this allows multiple screen resolutions without resorting to hacking the 2d or,
+           like some 3d engines do, make the 2d elements really 3d that are projected funny.
+       use_psyco indicates whether to try and use psyco if it is available, for a speed boost.
+       icon_image must be a string indicating the image to load from disk, or a pygame Surface to use for the window icon.
+       full_screen indicates whether the render screen is fullscreen or not
+       hwrender indicates whether hwrendering should be used for pygame operations
+       decorated indicates whether the display window should have a border and top bar or not"""
     pyggel.init(screen_size=(640,480)) #initialize everything
-    #for now, pyggel.init just calls pyggel.view init...
+    #for now, pyggel.init just calls pyggel.view.init...
 
     event_handler = pyggel.event.Handler()
 
