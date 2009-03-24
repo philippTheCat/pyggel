db732076413018ec9ce30dd9a0f33b5c045cfc94
RoeBros <RoeBros@29c87561-914b-0410-992d-b37967ad6221>
2009-03-24 19:43:32 +0000

    Commented the tut theme.

--- examples_and_tutorials/data/gui/theme.py
+++ examples_and_tutorials/data/gui/theme.py
@@ -1,56 +1,54 @@
-"Fonts":{
-    "default":{
-        "fontfile":None,
-        "fontsize":32,
-        "images":{"XD":"football.gif"}
+"Fonts":{ #create all the fonts the gui will use
+    "default":{ #font "default"
+        "fontfile":None, #None loads the system default, otherwise just make a .ttf file in the same dir
+        "fontsize":32, #size
+        "images":{"XD":"football.gif"} #embeddable images, must be in same dir
         }
     },
-"App":{
-    "font":"default"
+"App":{}, #Theme stuff for Apps, nothing is used ;)
+"Widget":{}, #idget, again, uses nothing because on it's own it does nothing!
+"Frame":{ #Frame
+    "size":(100,100), #default size for frames
+    "background-image":"base.png" #background image for the frame
     },
-"Widget":{},
-"Frame":{
-    "size":(100,100),
-    "background-image":"base.png"
-    },
-"Label":{
-    "font":"default",
-    "text":"label...",
+"Label":{ #Label
+    "font":"default", #what font to use?
+    "text":"label...", #default text
     "background-image":"base.png",
-    "font-color":(1,1,1,1),
-    "font-color-inactive":(1,1,1,.5),
-    "font-underline":False,
-    "font-italic":False,
-    "font-bold":False
+    "font-color":(1,1,1,1), #font color
+    "font-color-inactive":(1,1,1,.5), #font color when widget is inactive
+    "font-underline":False, #underline text?
+    "font-italic":False, #italicize text?
+    "font-bold":False #bold text?
     },
-"Button":{
+"Button":{ #Button
     "font":"default",
     "text":"button...",
     "background-image":"base.png",
-    "background-image-hover":"base.png",
+    "background-image-hover":"base.png", #different bg image for different states (regular, hover, click)
     "background-image-click":"base.png",
     "font-color":(1,1,1,1),
-    "font-color-hover":(0,1,0,1),
+    "font-color-hover":(0,1,0,1), #different font color for different states
     "font-color-click":(1,0,0,1),
     "font-underline":False,
     "font-italic":False,
     "font-bold":False,
-    "font-underline-hover":False,
+    "font-underline-hover":False, #different font attributes for different states
     "font-italic-hover":False,
     "font-bold-hover":False,
     "font-underline-click":False,
     "font-italic-click":False,
     "font-bold-click":False
     },
-"Checkbox":{
-    "background-image":"check_open.png",
-    "check-image":"check_closed.png"
+"Checkbox":{ #Checkbox
+    "background-image":"check_open.png", #image when not checked
+    "check-image":"check_closed.png" #image when checked
     },
-"Radio":{
+"Radio":{ #Radio
     "font":"default",
     "background-image":"base.png",
-    "option-background-image":"check_open.png",
-    "option-check-image":"check_closed.png",
+    "option-background-image":"check_open.png", #background image for checkboxes
+    "option-check-image":"check_closed.png", #check image for checkboxes
     "font-color":(1,1,1,1),
     "font-color-inactive":(1,1,1,.5),
     "font-underline":False,
@@ -70,8 +68,8 @@
     },
 "Input":{
     "font":"default",
-    "text":"input...",
-    "width":100,
+    "text":"input...", #start text
+    "width":100, #width of widget
     "background-image":"base.png",
     "font-color":(1,1,1,1),
     "font-color-inactive":(1,1,1,.5),
@@ -86,8 +84,8 @@
     "font-underline":False,
     "font-italic":False,
     "font-bold":False,
-    "title":"Window...",
-    "width":100,
+    "title":"Window...", #title of bar
+    "width":100, #width of bar
     "background-image":"base.png"
     },
 "Window":{
@@ -97,30 +95,30 @@
     "font-underline":False,
     "font-italic":False,
     "font-bold":False,
-    "size":(100,100),
-    "background-image":"base.png",
-    "movebar-background-image":"base.png"
+    "size":(100,100), #size of Frame, and [0] is width of MoveBar
+    "background-image":"base.png", #Frame background image
+    "movebar-background-image":"base.png" #MoveBar background image
     },
 "Menu":{
     "name":"menu...",
     "font":"default",
-    "font-color":(1,1,1,1),
+    "font-color":(1,1,1,1), #attributes for Button that activates menu
     "font-color-hover":(0,1,0,1),
     "font-color-click":(1,0,0,1),
     "background-image":"base.png",
     "background-image-hover":"base.png",
     "background-image-click":"base.png",
-    "menu-background-image":"base.png",
-    "option-background-image":"base.png",
+    "menu-background-image":"base.png", #background for menu frames
+    "option-background-image":"base.png", #attributes for each option (not sub-menu) button
     "option-background-image-hover":"base.png",
     "option-background-image-click":"base.png",
     "option-font-color":(1,1,1,1),
     "option-font-color-hover":(0,1,0,1),
     "option-font-color-click":(1,0,0,1),
-    "sub-background-image":"base.png",
+    "sub-background-image":"base.png", #attributes for each option (only sub-menu) button
     "sub-background-image-hover":"base.png",
     "sub-background-image-click":"base.png",
-    "sub-icon":"menu_icon.png",
+    "sub-icon":"menu_icon.png", #image for right/left arrows indicating sub-menu
     "sub-font-color":(0,0,1,1),
     "sub-font-color-hover":(0,1,1,1),
     "sub-font-color-click":(1,1,0,1),
